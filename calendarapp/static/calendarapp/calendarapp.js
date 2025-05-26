e6157faa76f2b2acfc75f95a51ab0b438ee0e822
lenkamac <mackova.lenka@protonmail.com>
2025-05-26 20:13:57 +0200

    style calendar

--- calendarapp/static/calendarapp/calendarapp.js
+++ calendarapp/static/calendarapp/calendarapp.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: calendarEventsUrl,  // We'll define this in template!
+        eventClick: function(info) {
+            alert('Event: ' + info.event.title);
+        },
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek, multiMonthYear',
+        }
+    });
+    calendar.render();
+});
 
