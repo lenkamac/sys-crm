556f786081d24f12af265717631fc8525b2bdfd2
lenkamac <mackova.lenka@protonmail.com>
2025-05-30 21:01:12 +0200

    work on dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from lead.models import Lead
+from client.models import Client
 
 
 app_name = 'dashboard'
@@ -8,4 +10,15 @@ app_name = 'dashboard'
 # Create your views here.
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    lead_count = Lead.objects.filter(created_by=request.user).count()
+    client_count = Client.objects.filter(created_by=request.user).count()
+    latest_leads = Lead.objects.filter(created_by=request.user).order_by('-created_at')[:5]
+    latest_clients = Client.objects.filter(created_by=request.user).order_by('-created_at')[:5]
+
+    context = {
+        'lead_count': lead_count,
+        'client_count': client_count,
+        'latest_leads': latest_leads,
+        'latest_clients': latest_clients,
+    }
+    return render(request, 'dashboard/dashboard.html', context)
